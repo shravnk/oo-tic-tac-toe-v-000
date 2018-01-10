74fa8485db89490d532f75c29e179465b366d5b5
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-10 06:45:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,7 +55,7 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board, index)
+  index.between?(0,8) && !position_taken?(index)
 end
 
 def turn
